56109351bd40683e7242bdb093391818366e4a9c
longovinicius <longo.vinicius@gmail.com>
2025-07-25 20:17:34 -0300

    fix: baudrate_i in SerialManager

--- SerialManager.vhd
+++ SerialManager.vhd
@@ -2,7 +2,7 @@
 --!
 --! \brief		
 --!
---! \author		Vinícius de Carvalho Monteiro Longo (longo@weg.net)
+--! \author		Vinícius de Carvalho Monteiro Longo (longo.vinicius@gmail.com)
 --! \date       24-07-2025
 --!
 --! \version    1.0
@@ -17,41 +17,39 @@
 --! \warning	None
 --!
 --! \note		Revisions:
---!				- 1.0	24-07-2025	<longo@weg.net>
+--!				- 1.0	24-07-2025	<longo.vinicius@gmail.com>
 --!				First revision.
---------------------------------------------------------------------------
--- Default libraries
---------------------------------------------------------------------------
---! \file       SerialManager.vhd
---! \date       24-07-2025
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use work.Solver_pkg.all; 
+use work.Solver_pkg.all;
 
 entity SerialManager is
     generic (
-        CLK_FREQ          : integer := 100_000_000; 
-        SEND_INTERVAL_US  : integer := 200;         
-        BAUD_RATE         : integer := 1_042_000     
+        CLK_FREQ          : integer := 100_000_000;
+        SEND_INTERVAL_US  : integer := 200;
+        BAUD_RATE         : integer := 1_042_000
     );
     port (
         sysclk            : in std_logic;
         reset_n           : in std_logic;
-
         data_in_i         : in fixed_point_data_t;
-
         tx_o              : out std_logic
     );
 end entity SerialManager;
 
 architecture rtl of SerialManager is
 
-
+    --------------------------------------------------------------------------
+    -- Constants
+    --------------------------------------------------------------------------
     constant SEND_INTERVAL_CYCLES : integer := (CLK_FREQ / 1_000_000) * SEND_INTERVAL_US;
-    constant BAUD_DIVISOR         : integer := CLK_FREQ / BAUD_RATE;
+    constant BAUD_DIVISOR_C       : integer := (CLK_FREQ / BAUD_RATE);
 
+    --------------------------------------------------------------------------
+    -- Signals
+    --------------------------------------------------------------------------
     type state_t is (S_IDLE, S_LATCH_DATA, S_SEND_BYTE, S_WAIT_DONE);
     signal fsm_state              : state_t := S_IDLE;
     signal timer_ctr              : integer range 0 to SEND_INTERVAL_CYCLES - 1 := 0;
@@ -61,11 +59,14 @@ architecture rtl of SerialManager is
     signal byte_to_send           : std_logic_vector(7 downto 0);
     signal uart_start             : std_logic := '0';
     signal uart_done              : std_logic;
+    signal baud_divisor_sig       : std_logic_vector(15 downto 0);
 
 begin
 
+    baud_divisor_sig <= std_logic_vector(to_unsigned(BAUD_DIVISOR_C, 16));
+
     --------------------------------------------------------------------------
-    -- Periodic Trigger Process
+    -- Data transfer trigger
     --------------------------------------------------------------------------
     Periodic_Trigger_Process: process(sysclk)
     begin
@@ -79,14 +80,14 @@ begin
                     timer_ctr <= timer_ctr + 1;
                 else
                     timer_ctr <= 0;
-                    send_trigger <= '1'; 
+                    send_trigger <= '1';
                 end if;
             end if;
         end if;
     end process;
 
     --------------------------------------------------------------------------
-    -- FSM
+    -- FSM 
     --------------------------------------------------------------------------
     Sequencer_FSM: process(sysclk)
     begin
@@ -102,16 +103,13 @@ begin
                         if send_trigger = '1' then
                             fsm_state <= S_LATCH_DATA;
                         end if;
-
                     when S_LATCH_DATA =>
                         latched_data <= data_in_i;
                         byte_index <= 0;
                         fsm_state <= S_SEND_BYTE;
-
                     when S_SEND_BYTE =>
                         uart_start <= '1';
                         fsm_state <= S_WAIT_DONE;
-
                     when S_WAIT_DONE =>
                         uart_start <= '0';
                         if uart_done = '1' then
@@ -127,9 +125,6 @@ begin
         end if;
     end process;
 
-    --------------------------------------------------------------------------
-    -- Slicing packet in 8 bit chunks
-    --------------------------------------------------------------------------
     with byte_index select
         byte_to_send <= latched_data(7 downto 0)   when 0,
                         latched_data(15 downto 8)  when 1,
@@ -140,21 +135,22 @@ begin
                         (others => '0')            when others;
 
     --------------------------------------------------------------------------
-    -- Uart_TX instantiation
+    -- Uart TX Instantiation
     --------------------------------------------------------------------------
     uart_tx_inst : entity work.Uart_TX
     generic map(
-        CLK_FREQ        => CLK_FREQ,
-        BAUD_RATE       => BAUD_RATE
+        DATA_WIDTH      => 8,
+        START_BIT       => '0',
+        STOP_BIT        => '1'
     )
     port map(
         sysclk          => sysclk,
         reset_n         => reset_n,
         start_i         => uart_start,
+        baudrate_i      => baud_divisor_sig, 
         data_i          => byte_to_send,
         tx_o            => tx_o,
         tx_done_o       => uart_done
     );
 
 end architecture rtl;
-
